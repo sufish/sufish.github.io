fef9ffcacf598871e3f56d9bda463d13ee72218b
肖玉萃 <xiaoyc@didikon.com>
2016-03-05 09:48:38 +0800

    首页

--- assets/js/base.js
+++ assets/js/base.js
@@ -2,6 +2,8 @@
  * Created by mxyue on 16/3/4.
  */
 var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100,
+    speed: 600,
+    //spaceBetween: 100,
+    autoplay: 5000,
+    pagination: '.swiper-pagination',
 });
